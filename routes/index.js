aa4ff3cff4dfbe8967937e92bfa166ad5d695878
xinxin <xinxin@rrkd.cn>
2016-06-28 10:03:07 +0800

    repair the code bug

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: '<h1>Express</h1>',
+  res.render('index', { title: '<h1>Express</h1>'
                         ,users:[{username:'zxx'},
                                 {username:'tank'},
                                 {username:'mark'}]
