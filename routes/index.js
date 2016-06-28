bba993ea3117abc526a51753cace39c08f719773
xinxin <xinxin@rrkd.cn>
2016-06-28 10:01:09 +0800

    edit the index

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.render('index', { title: '<h1>Express</h1>',
+                        ,users:[{username:'zxx'},
+                                {username:'tank'},
+                                {username:'mark'}]
+            });
 });
 
 module.exports = router;
